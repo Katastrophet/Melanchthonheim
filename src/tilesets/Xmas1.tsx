39726e1d2b538b0e79af3615ea7f50a5b4bc6eed
Sarah Kocher <sarah.kocher12@gmail.com>
2021-12-30 16:43:04 +0100

    Fixed and restructured stuff

---
+++ src/tilesets/Xmas1.tsx
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Xmas1" tilewidth="32" tileheight="32" tilecount="6" columns="3">
+ <image source="../../lpc_christmas_pack/Fireplace Fire - Burning.png" width="96" height="64"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="100"/>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="2" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
---
+++ src/tilesets/tileset1-repositioning.tsx
@@ -0,0 +1,8 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="tileset1-repositioning" tilewidth="32" tileheight="32" spacing="2" margin="1" tilecount="121" columns="11">
+ <properties>
+  <property name="tilesetCopyright">© 2021 WorkAdventure &lt;https://workadventu.re/&gt; 
+License: WORKADVENTURE SPECIFIC RESOURCES LICENSE (see LICENSE.assets file)</property>
+ </properties>
+ <image source="../assets/tileset1-repositioning.png" width="374" height="374"/>
+</tileset>
